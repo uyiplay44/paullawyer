be6e866466b57a70ec69efe009d924eddae40906
uyiplay44 <uyiplay44@hotmail.com>
2023-07-04 02:27:16 +0000

    bulding

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { Navbar } from "./components/Navbar/Navbar"
+
 
 function App() {
 
   return (
     <div>
-
+      <Navbar />
     </div>
   )
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import './Navbar.css'
+import Logo from "../../assets/logo1.png";
+import {HiHome} from 'react-icons/hi';
+
+export const Navbar = () => {
+  return (
+    <div className='Navbar_main'>
+      <div className='Nav_logo'>
+        <img src={Logo} alt="" />
+      </div>
+      <nav className='Nav-section'>
+        <ul className='Nav_contain'>
+          <li>
+            <span><HiHome />Home</span>
+            <span>About Us</span>
+            <span>Our Service</span>
+            <span>Contact</span>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
