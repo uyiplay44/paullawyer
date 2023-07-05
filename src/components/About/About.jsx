b7b71ff0001195d961b693e83969f97549c65ab0
uyiplay44 <uyiplay44@hotmail.com>
2023-07-05 00:09:57 +0000

    About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,8 +3,27 @@ import './About.css';
 export const About = () => {
   return (
     <div className='About_main'>
-      <div>
-        
+      <div className='About_container'>
+        <h1>ABOUT US</h1>
+        <div className=''>
+          <div>
+            <h3>
+              Founded in 2001, Rahman Ravelli is a conflict-free, market-leading financial crime, cross-border investigations and complex commercial litigation legal practice.
+            </h3>
+          </div>
+          <p>We are regularly called upon to provide strategic advice to corporations and individuals in national and international matters involving criminal or regulatory investigations, asset tracing and recovery, civil fraud and complex business disputes across a wide range of industries.
+
+            With London headquarters and a global reach, our integrated team includes experienced litigators qualified to practise in England and Wales, France and Germany.  Our network of trusted partner firms in key overseas jurisdictions ensures we can address legal matters for clients anywhere in the world.
+
+            Rahman Ravelli combines astute business intelligence and legal expertise with proactive, creative strategies and a determination to achieve the best possible outcome. We provide practical, innovative solutions to our clients’ commercial, regulatory and criminal litigation issues. It is an approach that has helped shape the law.
+
+            All of our matters are handled directly by a partner of the firm. This means that the very highest quality legal advice is devoted to protecting the interests of each and every client. Yet we minimise costs by adopting flexible fee structures and creating lean teams of agile, experienced senior lawyers who can efficiently and effectively address clients’ issues.
+
+            As a boutique practice offering top-tier legal services, we are regularly engaged by other law firms to provide independent advice to senior executives and to handle discrete, high-stakes legal matters in jurisdictions around the world. </p>
+          <div>
+            <p></p>
+          </div>
+        </div>
       </div>
     </div>
   )
