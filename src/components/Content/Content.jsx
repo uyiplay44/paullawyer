2c751f4fc827102908096885ee0969f328b013db
uyiplay44 <uyiplay44@hotmail.com>
2023-07-15 03:13:56 +0000

    continue part 2

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,50 +1,53 @@
 import './Content.css'
+import Photo5 from '../../assets/Photo5.jpg'
 
 export const Content = () => {
   return (
-    <div className='ContentBody_main'>
+    <>
       <div className="ContentContainer">
-        <div className="ContentImage">
-          <img src="" alt="" />
-        </div>
         <div className="ContentBio">
-          <h2>Your Lawyer</h2>
-        </div>
-        <div className="ContentInfo">
-          <article>
-          Introduction:
+          <div className="ContentImage">
+            <img src={Photo5} alt="" />
+          </div>
+          <div className="ContentInfo">
+            <article className='ContentIntro'>
+              <h2>Your Lawyer</h2>
+              Introduction:
+              <br/>
+              <br/>
+              Paul Holm is an experienced and highly trained attorney who works at Paul Holm Chambers, a reputable law firm in London. With a strong dedication to providing superior legal services, Paul Holm and the company have established a solid reputation for their competence, professionalism, and client-centered approach.
 
-Paul Holm is an experienced and highly trained attorney who works at Paul Holm Chambers, a reputable law firm in London. With a strong dedication to providing superior legal services, Paul Holm and the company have established a solid reputation for their competence, professionalism, and client-centered approach.
+              <br/>
+              <br/>
 
-Paul Holm has an exceptional educational background, having obtained a Master of Arts in Business Management and Legal Studies (MA) and Law with Computer Science from the University of Aberdeen (LLB) Kings College. Upon graduation, they went to law school & earned their Juris Doctor (J.D.). Paul Holm has also sought greater specialty in particular areas of law through significant continuing legal education.
+              Paul Holm has an exceptional educational background, having obtained a Master of Arts in Business Management and Legal Studies (MA) and Law with Computer Science from the University of Aberdeen (LLB) Kings College. Upon graduation, they went to law school & earned their Juris Doctor (J.D.). Paul Holm has also sought greater specialty in particular areas of law through significant continuing legal education.
 
-Practice Areas:
+              <br/>
+              <br/>
 
-At [Firm Name], [Lawyer's Name] and the team focus on a wide range of practice areas. Some of their core areas of expertise include:
-* 		Civil Litigation: [Description of civil litigation practice]
-* 		Corporate Law: [Description of corporate law practice]
-* 		Criminal Law: [Description of criminal law practice]
-* 		Family Law: [Description of family law practice]
-* 		Real Estate Law: [Description of real estate law practice]
-* 		Intellectual Property Law: [Description of intellectual property law practice]
-* 		Employment Law: [Description of employment law practice]
-* 		Immigration Law: [Description of immigration law practice]
+              Approach and Philosophy:
+              Paul Holm and the firm take pleasure in their client-centered approach to legal representation. They believe in developing strong relationships with their clients with trust, communication, and personal attention. Paul Holm takes the time to understand each clients individual circumstances, goals, and concerns, adapting their legal tactics to obtain the best possible results.
 
-Approach and Philosophy:
-Paul Holm and the firm take pleasure in their client-centered approach to legal representation. They believe in developing strong relationships with their clients with trust, communication, and personal attention. Paul Holm takes the time to understand each clients individual circumstances, goals, and concerns, adapting their legal tactics to obtain the best possible results.
+              <br/>
+              <br/>
 
-Significant Achievements and Accolades:
-Paul Holm has garnered recognised for their remarkable legal talents and has accomplished various notable successes during their career. Successful case results, high-profile clientele, major donors, leadership roles in the legal organization, or publication in legal journals are examples of these accomplishments.
+              Significant Achievements and Accolades:
+              Paul Holm has garnered recognised for their remarkable legal talents and has accomplished various notable successes during their career. Successful case results, high-profile clientele, major donors, leadership roles in the legal organization, or publication in legal journals are examples of these accomplishments.
+              <br/>
+              <br/>
 
-Client Testimonials:
-Clients of Paul Holm and Paul Holm Chamber often commend their professionalism, dedication, and results-oriented approach. They appreciate the firms responsiveness, clear communication, and thorough understanding of the law. Client testimonials may be included to illustrate the lawyer and firm’s positive impact on their clients’ lives.
+              Client Testimonials:
+              Clients of Paul Holm and Paul Holm Chamber often commend their professionalism, dedication, and results-oriented approach. They appreciate the firms responsiveness, clear communication, and thorough understanding of the law. Client testimonials may be included to illustrate the lawyer and firm’s positive impact on their clients’ lives.
 
-Conclusion:
-Paul Holm and Paul Holm Chamber are dedicated legal professionals committed to providing outstanding legal services to their clients. With their extensive experience, comprehensive understanding of the law, and client-centered approach, they continue to uphold their reputation as trusted advocates in their community.
+              <br/>
+              <br/>
 
-          </article>
+              Conclusion:
+              Paul Holm and Paul Holm Chamber are dedicated legal professionals committed to providing outstanding legal services to their clients. With their extensive experience, comprehensive understanding of the law, and client-centered approach, they continue to uphold their reputation as trusted advocates in their community.
+            </article>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,8 @@
 
-import { useState } from 'react';
 import Logo from '../../assets/Logo1.png';
 import './Footer.css';
 
 export const Footer = () => {
-  const [email, setEmail] = useState();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email)
-  }
 
   return (
     <div className='FooterMain'>
@@ -18,22 +11,20 @@ export const Footer = () => {
       </div>
       <div className="FooterSelect">
         <div className="FooterTaag">
-          <h2>Address</h2>
-          <p>Kingsley Napley LLP
-            20 Bonhill Street
-            London
-            EC2A 4DN </p>
+          <div className='Address'>
+            <h2>Address</h2>
+            <p>Kingsley Napley LLP
+              20 Bonhill Street
+              London
+              EC2A 4DN
+            </p>
+          </div>
           <div className='FooterContact'>
             <h3>Contact Details</h3>
             <p>Telephone: +44 (0)20 7814 1200</p>
             <p>Telephone: +44 (0)20 7490 2288</p>
           </div>
         </div>
-        <form className="FooterTaagAddress" onSubmit={handleSubmit}>
-          <label>Submit</label>
-          <input type="email" placeholder='Your Email' id='email' value={email} />
-        </form>
-          <button type='submit'>Submit</button>
       </div>
     </div>
   )
